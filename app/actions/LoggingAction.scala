0852563e47386808df2548b9dc14365eae5b204c
hideto0710 <h.inamura0710@gmail.com>
2016-01-12 22:19:02 +0900

    use andThen

--- app/actions/LoggingAction.scala
+++ app/actions/LoggingAction.scala
@@ -1,6 +1,7 @@
 package actions
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 import play.api.mvc.{Result, Request, ActionBuilder}
 import play.api.Logger
 
@@ -12,9 +13,11 @@ object LoggingAction extends ActionBuilder[Request] {
     val now = System.currentTimeMillis()
     Logger.info("Start action")
     val result = block(request)
-    result.foreach(r =>
-      Logger.info(s"End action\nDuration: ${System.currentTimeMillis()-now}, Result: ${r.toString()}")
-    )
-    result
+    result.andThen {
+      case Success(r) =>
+        Logger.info(s"End action\nDuration: ${System.currentTimeMillis()-now}, Result: ${r.toString}")
+      case Failure(t) =>
+        Logger.info(s"Failed action\nDuration: ${System.currentTimeMillis()-now}, Result: ${t.getMessage}")
+    }
   }
 }
